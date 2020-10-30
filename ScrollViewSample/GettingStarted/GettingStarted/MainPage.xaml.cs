250054de2821b9bf5d2e0f1a0058a08d42e90a0e
Eswaran T <eswaran.thirugnanasambandam@syncfusion.com>
2020-10-30 16:41:00 +0530

    File committed

---
+++ ScrollViewSample/GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void AllButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void SearchBar_SearchButtonPressed(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void SearchBar_Focused(System.Object sender, Xamarin.Forms.FocusEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var height = SearchGrid.Height;
+                await MainScroll.ScrollToAsync(0, height, true);
+            });
+        }
+
+        void SearchBar_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+        }
+    }
+
+    //public magicGradients : SfGradientView
+    //    {
+
+    //    }
+
+}
---
+++ ScrollViewSample/GettingStarted/GettingStarted/MyPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
